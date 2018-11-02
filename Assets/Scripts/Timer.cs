640f6ab56cc159ed39d7ec8b62daf285dc96d86d
Katy Mollenkopf <kmm5278@rit.edu>
2018-11-02 13:55:35 -0400

    Added a timer to the win text so that is appears when the bool on the script is enabled

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    private float timeLimit = 4;
+    public bool timerOn;
+    public Text text;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        timerOn = false;
+        text.enabled = false;
+    }
+    void Update ()
+    {
+        if (timerOn)
+        {
+            if (timeLimit > 1)
+            {
+                //Display the text
+                text.enabled = true;
+                // Decrease time Limit.
+                timeLimit -= Time.deltaTime;
+            }
+            else
+            {
+                timeLimit = 4;
+                timerOn = false;
+                text.enabled = false;
+            }
+        }
+    }
+}
